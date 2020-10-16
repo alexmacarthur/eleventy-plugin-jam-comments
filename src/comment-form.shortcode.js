ef504d4028532f7dc9f49365034219e4df2d4e31
Alex MacArthur <alex@macarthur.me>
2020-10-15 22:39:59 -0500

    Configure njk template to load correctly, drop in basic HTML.

--- src/comment-form.shortcode.js
+++ src/comment-form.shortcode.js
@@ -1,21 +1,9 @@
-const outdent = require("outdent")({ newline: " " });
+const path = require("path");
 const nunjucks = require("nunjucks");
 
-nunjucks.configure('./eleventy-plugin-jam-comments/views/');
-
-console.log("EHRE");
-console.log(process.cwd());
+nunjucks.configure(path.join(__dirname, 'views'));
 
 module.exports = (id) => {
-
-  return nunjucks.render('index');
-
-  return outdent`
-    <div class="video-wrapper">
-        <iframe src="https://www.youtube.com/embed/${id}"
-            frameborder="0" allowfullscreen
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture">
-
-        </iframe>
-    </div>`
+  console.log('hihey');
+  return nunjucks.render('index.njk');
 };
